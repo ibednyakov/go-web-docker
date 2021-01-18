bd92ba57750841d3ca238ab7e5a37d6a4f5d0077
ibednyakov <ivan.bednyakov@gmail.com>
2021-01-18 17:27:48 +0300

    Working JSON returning Bee Go service

--- go.mod
+++ go.mod
@@ -1,8 +1,11 @@
 module github.com/ibednyakov/go-web-docker
 
 require (
+	4d63.com/gochecknoinits v0.0.0-20200108094044-eb73b47b9fc4 // indirect
 	github.com/astaxie/beego v1.12.1
 	github.com/beego/bee v1.12.3 // indirect
 	github.com/shiena/ansicolor v0.0.0-20200904210342-c7312218db18 // indirect
 	github.com/stretchr/objx v0.1.1 // indirect
+	mvdan.cc/interfacer v0.0.0-20180901003855-c20040233aed // indirect
+	mvdan.cc/lint v0.0.0-20170908181259-adc824a0674b // indirect
 )
--- src/main.go
+++ src/main.go
@@ -3,54 +3,111 @@
 package main
 
 import (
-    "strconv"
-    "github.com/astaxie/beego"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/astaxie/beego"
 )
 
+const (
+	Users     = "Users"
+	User      = "User"
+	UserId    = "UserId"
+	Operation = ":operation"
+)
 
 func main() {
-    /* This would match routes like the following:
-       /sum/3/5
-       /product/6/23
-       ...
-    */
-    beego.Router("/:operation/:num1:int/:num2:int", &mainController{})
-    beego.Run()
+	/* This would match routes like the following:
+	   /sum/3/5
+	   /product/6/23
+	   ...
+	*/
+	beego.Router("/:operation/:num1:int/:num2:int", &mainController{})
+	beego.Run()
 }
 
 type mainController struct {
-    beego.Controller
+	beego.Controller
 }
 
+type Person struct {
+	Fn string
+	Ln string
+}
+type ColorGroup struct {
+	ID1    int
+	ID2    int
+	Name   string
+	Colors []string
+	P      Person `json:"Person"`
+}
 
 func (c *mainController) Get() {
 
-    //Obtain the values of the route parameters defined in the route above    
-    operation := c.Ctx.Input.Param(":operation")
-    num1, _ := strconv.Atoi(c.Ctx.Input.Param(":num1"))
-    num2, _ := strconv.Atoi(c.Ctx.Input.Param(":num2"))
+	//Obtain the values of the route parameters defined in the route above
+	operation := c.Ctx.Input.Param(Operation)
+	num1, _ := strconv.Atoi(c.Ctx.Input.Param(":num1"))
+	num2, _ := strconv.Atoi(c.Ctx.Input.Param(":num2"))
 
-    //Set the values for use in the template
-    c.Data["operation"] = operation
-    c.Data["num1"] = num1
-    c.Data["num2"] = num2
-    c.TplName = "result.html"
+	//Set the values for use in the template
+	c.Data["operation"] = operation
+	c.Data["num1"] = num1
+	c.Data["num2"] = num2
+	c.TplName = "result.html"
 
-    // Perform the calculation depending on the 'operation' route parameter
-    switch operation {
-    case "sum":
-        c.Data["result"] = add(num1, num2)
-    case "product":
-        c.Data["result"] = multiply(num1, num2)
-    default:
-        c.TplName = "invalid-route.html"
-    }
+	// Perform the calculation depending on the 'operation' route parameter
+	switch operation {
+	case "sum":
+		c.Data["result"] = add(num1, num2)
+	case "product":
+		c.Data["result"] = multiply(num1, num2)
+	case "json":
+		per := Person{Fn: "John",
+			Ln: "Doe",
+		}
+		group := ColorGroup{
+			ID1:    num1,
+			ID2:    num2,
+			Name:   "Reds",
+			Colors: []string{"Crimson", "Red", "Ruby", "Maroon"},
+			P:      per,
+		}
+		b, err := json.Marshal(group)
+		if err != nil {
+			fmt.Println("error:", err)
+		} else {
+			c.Data["json"] = &group
+			c.ServeJSON()
+			os.Stdout.Write(b)
+		}
+	default:
+		c.TplName = "invalid-route.html"
+	}
+}
+
+func (c *mainController) Put() {
+	operation := c.Ctx.Input.Param(Operation)
+	switch operation {
+	case Users:
+	default:
+		fmt.Println(operation)
+	}
+}
+
+func (c *mainController) Post() {
+	operation := c.Ctx.Input.Param(Operation)
+	switch operation {
+	default:
+		fmt.Println(operation)
+	}
 }
 
 func add(n1, n2 int) int {
-    return n1 + n2
+	return n1 + n2
 }
 
 func multiply(n1, n2 int) int {
-    return n1 * n2
+	return n1 * n2
 }
